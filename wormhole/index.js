43e3982d09de356af09b5dd1e26ba6885f8d98ed
skengrek <armand.masse69@gmail.com>
2024-11-10 17:42:48 +0100

    feat(wormhole): Change the look of the tube and add boxes

--- wormhole/index.js
+++ wormhole/index.js
@@ -18,18 +18,53 @@ const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 4;
 
 const scene = new THREE.Scene();
-
+scene.fog = new THREE.FogExp2(0x000000, 0.3)
 const geometry = new THREE.TubeGeometry(spline, 222, 0.65, 16, true);
-const material = new THREE.MeshStandardMaterial({
+const material = new THREE.MeshBasicMaterial({
     color:0xFF0000,
     side: THREE.DoubleSide,
     wireframe: true,
 });
-const line = new THREE.Mesh(geometry, material);
-scene.add(line);
+const tube = new THREE.Mesh(geometry, material);
+// scene.add(tube);
+
+const edges = new THREE.EdgesGeometry(geometry, 0.2);
+const edgesMat = new THREE.LineBasicMaterial({color: 0x00FF00});
+const tubeLine = new THREE.LineSegments(edges, edgesMat)
+scene.add(tubeLine);
+
+const numBoxes = 100;
+const size = 0.075;
+const boxGeo = new THREE.BoxGeometry(size, size, size);
+for (let i = 0; i < numBoxes; i += 1) {
+  const boxMat = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    wireframe: true
+  });
+  const box = new THREE.Mesh(boxGeo, boxMat);
+  const p = (i / numBoxes + Math.random() * 0.1) % 1;
+  const pos = geometry.parameters.path.getPointAt(p);
+  pos.x += Math.random() - 0.4;
+  pos.z += Math.random() - 0.4;
+  box.position.copy(pos);
+  const rote = new THREE.Vector3(
+    Math.random() * Math.PI,
+    Math.random() * Math.PI,
+    Math.random() * Math.PI
+  );
+  box.rotation.set(rote.x, rote.y, rote.z);
+  const edges = new THREE.EdgesGeometry(boxGeo, 0.2);
+  const color = new THREE.Color().setHSL(0.7 - p, 1, 0.5);
+  const lineMat = new THREE.LineBasicMaterial({ color });
+  const boxLines = new THREE.LineSegments(edges, lineMat);
+  boxLines.position.copy(pos);
+  boxLines.rotation.set(rote.x, rote.y, rote.z);
+  scene.add(boxLines);
+}
+
 
 function updateCamera(t) {
-    const time = t * 0.2;
+    const time = t * 0.15;
     const looptime = 10 * 1000;
     const p = (time % looptime) / looptime;
     const pos = geometry.parameters.path.getPointAt(p);
@@ -38,13 +73,6 @@ function updateCamera(t) {
     console.log(pos, lookAt)
     camera.lookAt(lookAt);
 }
-
-
-
-// Lights
-const hemiLight = new THREE.HemisphereLight(0xffffbb, 0x000000, 2);
-hemiLight.position.set(0, 5, 0);
-scene.add(hemiLight);
 
 function animate(t=0){
     requestAnimationFrame(animate);
