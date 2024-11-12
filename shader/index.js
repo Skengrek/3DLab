5ff7e9f5fb4566c544dd5c1c7f4a22d31f4c313d
skengrek <armand.masse69@gmail.com>
2024-11-12 15:47:04 +0100

    feat(shader): Added a shader test example

---
+++ shader/index.js
@@ -0,0 +1,169 @@
+import * as THREE from "three";
+import {OrbitControls} from "jsm/controls/OrbitControls.js"
+
+const width = window.innerWidth
+const height = window.innerHeight
+
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+});
+renderer.setSize(width, height);
+document.body.appendChild(renderer.domElement);
+
+const fov = 75;
+const aspect = width/height;
+const near = 0.1;
+const far = 10;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 4;
+
+const scene = new THREE.Scene();
+
+
+const vertexShader = `varying vec2 vUv;
+void main() {
+    vUv = vec2( uv.x, uv.y );
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+
+}`
+
+const fragmentShader=`
+precision highp float;
+uniform float iTime;
+uniform vec3 iResolution;
+
+float gTime = 0.;
+const float REPEAT = 5.0;
+mat2 rot(float a) {
+	float c = cos(a), s = sin(a);
+	return mat2(c,s,-s,c);
+}
+
+float sdBox( vec3 p, vec3 b )
+{
+	vec3 q = abs(p) - b;
+	return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
+}
+
+float box(vec3 pos, float scale) {
+	pos *= scale;
+	float base = sdBox(pos, vec3(.4,.4,.1)) /1.5;
+	pos.xy *= 5.;
+	pos.y -= 3.5;
+	pos.xy *= rot(.75);
+	float result = -base;
+	return result;
+}
+
+float box_set(vec3 pos, float iTime) {
+	vec3 pos_origin = pos;
+	pos = pos_origin;
+	pos .y += sin(gTime * 0.4) * 2.5;
+	pos.xy *=   rot(.8);
+	float box1 = box(pos,2. - abs(sin(gTime * 0.4)) * 1.5);
+	pos = pos_origin;
+	pos .y -=sin(gTime * 0.4) * 2.5;
+	pos.xy *=   rot(.8);
+	float box2 = box(pos,2. - abs(sin(gTime * 0.4)) * 1.5);
+	pos = pos_origin;
+	pos .x +=sin(gTime * 0.4) * 2.5;
+	pos.xy *=   rot(.8);
+	float box3 = box(pos,2. - abs(sin(gTime * 0.4)) * 1.5);	
+	pos = pos_origin;
+	pos .x -=sin(gTime * 0.4) * 2.5;
+	pos.xy *=   rot(.8);
+	float box4 = box(pos,2. - abs(sin(gTime * 0.4)) * 1.5);	
+	pos = pos_origin;
+	pos.xy *=   rot(.8);
+	float box5 = box(pos,.5) * 6.;	
+	pos = pos_origin;
+	float box6 = box(pos,.5) * 6.;	
+	float result = max(max(max(max(max(box1,box2),box3),box4),box5),box6);
+	return result;
+}
+
+float map(vec3 pos, float iTime) {
+	vec3 pos_origin = pos;
+	float box_set1 = box_set(pos, iTime);
+
+	return box_set1;
+}
+
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
+	vec2 p = (fragCoord.xy * 2. - iResolution.xy) / min(iResolution.x, iResolution.y);
+	vec3 ro = vec3(0., -0.2 ,iTime * 4.);
+	vec3 ray = normalize(vec3(p, 1.5));
+	ray.xy = ray.xy * rot(sin(iTime * .03) * 5.);
+	ray.yz = ray.yz * rot(sin(iTime * .05) * .2);
+	float t = 0.1;
+	vec3 col = vec3(0.);
+	float ac = 0.0;
+
+
+	for (int i = 0; i < 99; i++){
+		vec3 pos = ro + ray * t;
+		pos = mod(pos-2., 4.) -2.;
+		gTime = iTime -float(i) * 0.01;
+		
+		float d = map(pos, iTime);
+
+		d = max(abs(d), 0.01);
+		ac += exp(-d*23.);
+
+		t += d* 0.55;
+	}
+
+	col = vec3(ac * 0.02);
+
+	col +=vec3(0.,0.2 * abs(sin(iTime)),0.5 + sin(iTime) * 0.2);
+
+
+	fragColor = vec4(col ,1.0 - t * (0.02 + 0.02 * sin (iTime)));
+}
+ 
+void main() {
+     mainImage(gl_FragColor, gl_FragCoord.xy);
+ }`
+
+
+const uniforms = {
+    iTime: { value: 0 },
+    iResolution:  { value: new THREE.Vector3() },
+  };
+
+// Basic object
+const geometry = new THREE.PlaneGeometry(25, 50)
+const mat = new THREE.ShaderMaterial({
+    fragmentShader, vertexShader, uniforms
+})
+const sphere = new THREE.Mesh(geometry, mat)
+scene.add(sphere)
+
+
+// mesh
+// const meshMat = new THREE.MeshStandardMaterial({
+//     color: 0x0000ff,
+//     wireframe: true
+// })
+// const mesh = new THREE.Mesh(geometry, meshMat)
+// mesh.scale.addScalar(0.01)
+// scene.add(mesh)
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000, 0.9)
+scene.add(hemiLight);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+
+function animate(t=0){
+    requestAnimationFrame(animate);
+    const canvas = renderer.domElement;
+    uniforms.iResolution.value.set(canvas.width, canvas.height, 1);
+    uniforms.iTime.value = t*0.001;
+
+    controls.update()
+    renderer.render(scene, camera)
+}
+animate()
